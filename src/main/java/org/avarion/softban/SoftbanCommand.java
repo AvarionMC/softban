03df1e82f2ddc7466b12a019cec98fae7158a6a6
Steven Van Ingelgem <steven@vaningelgem.be>
2024-08-20 14:07:19 +0200

    Saving

--- src/main/java/org/avarion/softban/SoftbanCommand.java
+++ src/main/java/org/avarion/softban/SoftbanCommand.java
@@ -1,19 +1,20 @@
 package org.avarion.softban;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SoftbanCommand implements TabExecutor {
+    private final Map<String, Long> confirmationMap = new HashMap<>();
+
     SoftbanCommand() {
         registerMyself();
     }
@@ -47,24 +48,94 @@ public class SoftbanCommand implements TabExecutor {
             return false;
         }
 
-        if (args.length!=2) {
+        if (args.length < 2) {
             return false;
         }
 
+        String subCommand = args[0].toLowerCase();
+        String username = args[1];
+
+        return switch (subCommand) {
+            case "set" -> {
+                if (args.length!=3) {
+                    yield false;
+                }
+                yield handleSetCommand(sender, username, args[2]);
+            }
+            case "remove" -> handleRemoveCommand(sender, username);
+            default -> false;
+        };
+    }
+
+    private boolean handleSetCommand(CommandSender sender, String username, String sLevel) {
         int level;
         try {
-            level = parseLevel(args[1]);
+            level = parseLevel(sLevel);
         } catch (IllegalArgumentException e) {
             sender.sendMessage("Invalid level. Use a number between 1 and 5.");
             return false;
         }
 
-        if (Softban.plugin.handleOnlinePlayer(Bukkit.getPlayer(args[0]), level) || Softban.plugin.handleOfflinePlayer(args[0], level)) {
-            sender.sendMessage("Soft ban level set to " + level + " for player " + args[0]);
-            return true;
+        Player onlinePlayer = Bukkit.getPlayer(username);
+        if (onlinePlayer!=null) {
+            return Softban.plugin.handleOnlinePlayer(onlinePlayer, level);
         }
 
-        return false;
+        handleOfflinePlayerSet(sender, username, level);
+        return true;
+    }
+
+    private void handleOfflinePlayerSet(@NotNull CommandSender sender, String username, int level) {
+        String confirmationKey = sender.getName() + ":" + username + ":set:" + level;
+
+        if (confirmationMap.containsKey(confirmationKey) && System.currentTimeMillis() - confirmationMap.get(confirmationKey) < 15000) {
+            confirmationMap.remove(confirmationKey);
+            if (Softban.plugin.handleOfflinePlayer(username, level)) {
+                sender.sendMessage("Soft ban level set to " + level + " for offline player " + username);
+            }
+        }
+        else {
+            confirmationMap.put(confirmationKey, System.currentTimeMillis());
+            sendConfirmationMessage(sender, "/softban set " + username + " " + level,
+                    "Confirm setting soft ban level " + level + " for offline player " + username
+            );
+        }
+    }
+
+    private boolean handleRemoveCommand(CommandSender sender, String username) {
+        Player onlinePlayer = Bukkit.getPlayer(username);
+        if (onlinePlayer!=null) {
+            return Softban.plugin.handleOnlinePlayer(onlinePlayer, 0);
+        }
+
+        handleOfflinePlayerRemove(sender, username);
+        return true;
+    }
+
+    private void handleOfflinePlayerRemove(@NotNull CommandSender sender, String username) {
+        String confirmationKey = sender.getName() + ":" + username + ":remove";
+        if (confirmationMap.containsKey(confirmationKey) && System.currentTimeMillis() - confirmationMap.get(confirmationKey) < 15000) {
+            confirmationMap.remove(confirmationKey);
+            if (Softban.plugin.handleOfflinePlayer(username, 0)) {
+                sender.sendMessage("Soft ban removed for offline player " + username);
+            }
+        }
+        else {
+            confirmationMap.put(confirmationKey, System.currentTimeMillis());
+            sendConfirmationMessage(sender, "/softban remove " + username, "Confirm removing soft ban for offline player " + username);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void sendConfirmationMessage(CommandSender sender, String command, String message) {
+        if (sender instanceof Player player) {
+            TextComponent component = new TextComponent(message + " (Click to confirm)");
+            component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command));
+            player.spigot().sendMessage(component);
+        }
+        else {
+            sender.sendMessage(message + " (Run the command again within 15 seconds to confirm)");
+        }
     }
 
     @Override
@@ -75,41 +146,20 @@ public class SoftbanCommand implements TabExecutor {
 
         List<String> completions = new ArrayList<>();
         if (args.length==1) {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                completions.add(player.getName());
-            }
-
+            completions.addAll(Arrays.asList("set", "remove"));
         }
         else if (args.length==2) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (player.getName().toLowerCase().startsWith(args[1].toLowerCase())) {
+                    completions.add(player.getName());
+                }
+            }
+        }
+        else if (args.length==3 && args[0].equalsIgnoreCase("set")) {
             for (int i = 1; i <= 5; i++) {
                 completions.add(String.valueOf(i));
             }
         }
         return completions;
     }
-
-    public void updateInventoryRestrictions(Player player, int newLevel) {
-        int oldLevel = Utils.getSoftbanLevel(player);
-
-        // Remove old restrictions
-        if (oldLevel > 0) {
-            for (int i = 0; i < oldLevel * 2 && i < player.getInventory().getSize(); i++) {
-                ItemStack item = player.getInventory().getItem(i);
-                if (item!=null && item.getType()==Material.BARRIER) {
-                    player.getInventory().setItem(i, null);
-                }
-            }
-        }
-
-        // Apply new restrictions
-        if (newLevel > 0) {
-            int slotsToBlock = newLevel * 2;
-            for (int i = 0; i < slotsToBlock && i < player.getInventory().getSize(); i++) {
-                player.getInventory().setItem(i, new ItemStack(Material.BARRIER));
-            }
-        }
-
-        player.updateInventory();
-    }
-
 }
